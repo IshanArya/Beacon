f993a15095a3ba7c48d226008e9224b1dedb344e
Brian Model <brian.model@alice-app.com>
2018-09-08 13:28:42 -0400

    added list activity things

---
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.hackathon.fellas.homedepotapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
