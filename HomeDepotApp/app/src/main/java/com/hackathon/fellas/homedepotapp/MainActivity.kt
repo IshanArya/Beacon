1d30d069c64e6139f4580004d7d11d1f719edafc
Rahul Bhethanabotla <agentrmb@gmail.com>
2018-09-08 12:07:38 -0400

    added main menu xml and kt

--- HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/MainActivity.kt
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/MainActivity.kt
@@ -2,11 +2,30 @@ package com.hackathon.fellas.homedepotapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helpButton.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        listButton.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
+
+        directionButton.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
