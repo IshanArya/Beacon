4d6965e995eb3c2b7bef971aa8faab541dd8ff6f
Kyle Stachowicz <kylestach99@gmail.com>
2018-09-08 18:24:50 -0400

    Cleanup

--- HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/NavigationActivity.kt
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/NavigationActivity.kt
@@ -3,7 +3,6 @@ package com.hackathon.fellas.homedepotapp
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -12,16 +11,10 @@ import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
-import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_navigation.*
-import kotlinx.android.synthetic.main.activity_navigation.returnHomeButton
 import java.lang.Math.abs
-import java.util.*
 
 
 class NavigationActivity : AppCompatActivity(), SensorEventListener {
@@ -30,7 +23,6 @@ class NavigationActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
-            Log.v("STEPS", event.values[0].toString() + " " + stepsAtChange)
             steps = event.values[0].toInt()
             if (direction != Direction.FORWARD) {
                 val stepsChange = steps - stepsAtChange
@@ -59,26 +51,19 @@ class NavigationActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
-        returnHomeButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
         navigator.startNavigation()
         val stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
         sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL)
 
-        if (checkSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (checkSelfPermission(Manifest.permission.BLUETOOTH)
+                == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
             startBluetoothScan()
             Log.v("PERMS", "Bluetooth permission granted")
         } else {
             Log.e("PERMS", "Permission not granted for bluetooth")
         }
-    }
-
-    fun onButtonPressed(view: View) {
-        var button = view as? Button
-        Log.i("button text", button?.getText().toString())
     }
 
     private lateinit var bluetoothAdapter: BluetoothAdapter
@@ -106,8 +91,9 @@ class NavigationActivity : AppCompatActivity(), SensorEventListener {
 
         bluetoothAdapter.startLeScan { device, signalStrength, scanRecord ->
             if (device.name != null && device.name.contains("Beacon")) {
-                Log.v("BTLE: ", "Device ${device.name} with RSSI $signalStrength")
-                val beaconId = Character.getNumericValue(device.name.get(device.name.indexOf("Beacon ") + "Beacon ".length)) - 1
+                val beaconId = Character.getNumericValue(
+                        device.name.get(device.name.indexOf("Beacon ") + "Beacon ".length)
+                ) - 1
                 navigator.updateSignalReading(beaconId, signalStrength)
             }
         }
