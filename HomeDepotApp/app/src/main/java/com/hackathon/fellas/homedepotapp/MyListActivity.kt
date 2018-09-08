1d50529214d684b4c86cc6bd77d483295dfcfe9c
IshanArya <ishan.arya130@gmail.com>
2018-09-08 18:35:22 -0400

    Added my list

---
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/MyListActivity.kt
@@ -0,0 +1,44 @@
+package com.hackathon.fellas.homedepotapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MyListActivity : AppCompatActivity() {
+
+    lateinit var myList: ListView
+
+    /**
+     * Taken from SharedData.tools (if tools is changed, this will break!)
+     */
+    val toolResults: MutableList<String> = mutableListOf(
+            "Multi-bit Screwdriver",
+            "Steel Drilling Hammer",
+            "Sinker Nails",
+            "Fiberglass Claw Hammer"
+    )
+    lateinit var toolAdapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_list)
+
+        myList = findViewById(R.id.my_list)
+        toolAdapter = ArrayAdapter(this, R.layout.tool_item, R.id.tool_name, toolResults)
+
+        myList.adapter = toolAdapter
+
+        myList.setOnItemClickListener{parent, view, position, id ->
+            var itemKey: String = myList.getItemAtPosition(position) as String
+            Toast.makeText(this, SharedData.tools[itemKey]?.item, Toast.LENGTH_SHORT).show()
+//            val intent: Intent = Intent(this, NavigationActivity::class.java).apply {
+//                putExtra("Node", SharedData.tools[itemKey])
+//            }
+//            startActivity(intent)
+        }
+
+    }
+
+}
--- HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/SearchActivity.kt
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.hackathon.fellas.homedepotapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
@@ -15,19 +14,6 @@ class SearchActivity : AppCompatActivity(), TextWatcher {
     lateinit var searchResults: ListView
     lateinit var searchBox: EditText
 
-    val tools: Map<String, BluetoothNode> = mapOf<String, BluetoothNode>(
-            Pair("Fiberglass Claw Hammer", BluetoothNode.NODE1),
-            Pair("Steel Drilling Hammer", BluetoothNode.NODE1),
-            Pair("Mallet Hammer", BluetoothNode.NODE1),
-            Pair("Diamond Tip Magnet Screwdriver", BluetoothNode.NODE2),
-            Pair("Multi-bit Screwdriver", BluetoothNode.NODE2),
-            Pair("Phillips Head Screwdriver", BluetoothNode.NODE2),
-            Pair("Panel Board Nails", BluetoothNode.NODE3),
-            Pair("Sinker Nails", BluetoothNode.NODE3),
-            Pair("Smooth Shank Common Nails", BluetoothNode.NODE3),
-            Pair("Drywall Screws", BluetoothNode.NODE4),
-            Pair("Deck Screws", BluetoothNode.NODE4)
-    )
     val toolResults: MutableList<String> = mutableListOf()
     lateinit var toolAdapter: ArrayAdapter<String>
 
@@ -37,17 +23,17 @@ class SearchActivity : AppCompatActivity(), TextWatcher {
         searchBox = findViewById(R.id.search_box)
         searchResults = findViewById(R.id.search_results)
         searchBox.addTextChangedListener(this)
-        toolAdapter = ArrayAdapter(this, R.layout.search_result, R.id.tool_name, toolResults)
+        toolAdapter = ArrayAdapter(this, R.layout.tool_item, R.id.tool_name, toolResults)
 
         searchResults.adapter = toolAdapter
 
         searchResults.setOnItemClickListener{parent, view, position, id ->
             var itemKey: String = searchResults.getItemAtPosition(position) as String
-//            Toast.makeText(this, tools[itemKey]?.item, Toast.LENGTH_SHORT).show()
-            val intent: Intent = Intent(this, NavigationActivity::class.java).apply {
-                putExtra("Node", tools[itemKey])
-            }
-            startActivity(intent)
+            Toast.makeText(this, SharedData.tools[itemKey]?.item, Toast.LENGTH_SHORT).show()
+//            val intent: Intent = Intent(this, NavigationActivity::class.java).apply {
+//                putExtra("Node", SharedData.tools[itemKey])
+//            }
+//            startActivity(intent)
         }
     }
 
@@ -61,7 +47,7 @@ class SearchActivity : AppCompatActivity(), TextWatcher {
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         if(s != null) {
             toolResults.clear()
-            for((key, value) in tools) {
+            for((key, value) in SharedData.tools) {
                 if(key.contains(s, true)) {
                     toolResults.add(key)
                 }
