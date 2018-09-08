88e20dace48f74562c3392902d76190570e512a8
IshanArya <ishan.arya130@gmail.com>
2018-09-08 17:03:57 -0400

    Added sending item select

---
+++ HomeDepotApp/app/src/main/java/com/hackathon/fellas/homedepotapp/BluetoothNode.kt
@@ -0,0 +1,8 @@
+package com.hackathon.fellas.homedepotapp
+
+enum class BluetoothNode(val item: String) {
+    NODE1("hammer"),
+    NODE2("screwdriver"),
+    NODE3("nails"),
+    NODE4("screws")
+}
